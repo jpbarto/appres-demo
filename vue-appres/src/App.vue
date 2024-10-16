11e8cb43435726259cc9e8c90f01f83584a7cf2f
jpbarto <jason.p.barto@gmail.com>
2024-10-16 01:25:30 +0100

    tried primevue but ran into js issues in the browser, trying vite to fix

--- vue-appres/src/App.vue
+++ vue-appres/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <header>Resilience Playground</header>
+  <!--
   <div id="console">
     <div id="statsConsole">
       <LoadStats :stats-board="loadStatsBoard" :worker="loadgen" />
@@ -8,76 +9,82 @@
       <node-stats :stats-board="appStatsBoard" :worker="appsvr" />
       <node-stats :stats-board="dataStatsBoard" :worker="datasvr" />
     </div>
+  </div> -->
+  <div>
+    <p>
+      Experiment 1: Steady state 
+      Set the load generator to 20 and watch as the system achieves a steady state.
+      <PrimeButton label="Now Hear This!"/>
+    </p>
   </div>
 </template>
 
-<script>
+<script setup>
 // import { ref } from 'vue';
+// import NodeStats from './components/NodeStats.vue';
+// import LoadStats from './components/LoadStats.vue';
+// import * as jsNetwork from './assets/js/Network.js';
+// import * as StatsMan from './assets/js/StatsMan.js';
 
-import NodeStats from './components/NodeStats.vue'
-import LoadStats from './components/LoadStats.vue';
-import * as jsNetwork from './assets/js/Network.js';
-import * as StatsMan from './assets/js/StatsMan.js';
+// const network = new jsNetwork.Network(1, 1);
 
-const network = new jsNetwork.Network(1, 1);
+// const datasvr = new Worker(new URL('./assets/js/DataServer.js', import.meta.url));
+// datasvr.nodeDisplayName = 'Data Server';
+// datasvr.nodeName = 'data-server';
+// const appsvr = new Worker(new URL('./assets/js/AppServer.js', import.meta.url));
+// appsvr.nodeDisplayName = 'App Server';
+// appsvr.nodeName = 'app-server';
+// const websvr = new Worker(new URL('./assets/js/WebServer.js', import.meta.url));
+// websvr.nodeDisplayName = 'Web Server';
+// websvr.nodeName = 'web-server';
+// const gateway = new Worker(new URL('./assets/js/GatewayServer.js', import.meta.url));
+// gateway.nodeDisplayName = 'Gateway';
+// gateway.nodeName = 'gateway';
+// const loadGen = new Worker(new URL('./assets/js/LoadGenerator.js', import.meta.url));
+// loadGen.nodeDisplayName = 'Load Generator';
+// loadGen.nodeName = 'load-generator';
 
-const datasvr = new Worker(new URL('./assets/js/DataServer.js', import.meta.url));
-datasvr.nodeDisplayName = 'Data Server';
-datasvr.nodeName = 'data-server';
-const appsvr = new Worker(new URL('./assets/js/AppServer.js', import.meta.url));
-appsvr.nodeDisplayName = 'App Server';
-appsvr.nodeName = 'app-server';
-const websvr = new Worker(new URL('./assets/js/WebServer.js', import.meta.url));
-websvr.nodeDisplayName = 'Web Server';
-websvr.nodeName = 'web-server';
-const gateway = new Worker(new URL('./assets/js/GatewayServer.js', import.meta.url));
-gateway.nodeDisplayName = 'Gateway';
-gateway.nodeName = 'gateway';
-const loadGen = new Worker(new URL('./assets/js/LoadGenerator.js', import.meta.url));
-loadGen.nodeDisplayName = 'Load Generator';
-loadGen.nodeName = 'load-generator';
+// network.addNode(datasvr.nodeName, datasvr);
+// network.addNode(appsvr.nodeName, appsvr);
+// network.addNode(websvr.nodeName, websvr);
+// network.addNode(gateway.nodeName, gateway);
+// network.addNode(loadGen.nodeName, loadGen);
 
-network.addNode(datasvr.nodeName, datasvr);
-network.addNode(appsvr.nodeName, appsvr);
-network.addNode(websvr.nodeName, websvr);
-network.addNode(gateway.nodeName, gateway);
-network.addNode(loadGen.nodeName, loadGen);
+// const statsMan = new StatsMan.StatsMan();
+// statsMan.addNode(datasvr);
+// statsMan.addNode(appsvr);
+// statsMan.addNode(websvr);
+// statsMan.addNode(gateway);
+// statsMan.addNode(loadGen);
+// // const loadStatsBoard = statsMan.loadStatsBoard;
+// const dataStatsBoard = statsMan.nodeStats[datasvr.nodeName];
+// const appStatsBoard = statsMan.nodeStats[appsvr.nodeName];
+// const webStatsBoard = statsMan.nodeStats[websvr.nodeName];
+// const gwStatsBoard = statsMan.nodeStats[gateway.nodeName];
+// const loadStatsBoard = statsMan.nodeStats[loadGen.nodeName];
 
-const statsMan = new StatsMan.StatsMan();
-statsMan.addNode(datasvr);
-statsMan.addNode(appsvr);
-statsMan.addNode(websvr);
-statsMan.addNode(gateway);
-statsMan.addNode(loadGen);
-// const loadStatsBoard = statsMan.loadStatsBoard;
-const dataStatsBoard = statsMan.nodeStats[datasvr.nodeName];
-const appStatsBoard = statsMan.nodeStats[appsvr.nodeName];
-const webStatsBoard = statsMan.nodeStats[websvr.nodeName];
-const gwStatsBoard = statsMan.nodeStats[gateway.nodeName];
-const loadStatsBoard = statsMan.nodeStats[loadGen.nodeName];
-
-export default {
-  name: 'App',
-  components: {
-    NodeStats,
-    LoadStats,
-  },
-  data() {
-    return {
-      // loadStatsBoard: statsMan.nodeStats['load-generator'],
-      gwStatsBoard: gwStatsBoard,
-      gateway: gateway,
-      webStatsBoard: webStatsBoard,
-      websvr: websvr,
-      appStatsBoard: appStatsBoard,
-      appsvr: appsvr,
-      dataStatsBoard: dataStatsBoard,
-      datasvr: datasvr,
-      loadStatsBoard: loadStatsBoard,
-      loadgen: loadGen
-    }
-  }
-}
+// export default {
+//   name: 'App',
+//   components: {
+//     NodeStats,
+//     LoadStats,
+//   },
+//   data() {
+//     return {
+//       // loadStatsBoard: statsMan.nodeStats['load-generator'],
+//       gwStatsBoard: gwStatsBoard,
+//       gateway: gateway,
+//       webStatsBoard: webStatsBoard,
+//       websvr: websvr,
+//       appStatsBoard: appStatsBoard,
+//       appsvr: appsvr,
+//       dataStatsBoard: dataStatsBoard,
+//       datasvr: datasvr,
+//       loadStatsBoard: loadStatsBoard,
+//       loadgen: loadGen
+//     }
+//   }
+// }
 </script>
 
 <style>
--- vue-appres/src/main.js
+++ vue-appres/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import App from './App.vue';
+import PrimeButton from 'primevue/button';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.component('PrimeButton', PrimeButton);
+
+app.mount('#app');
