f7f54fb9658806ff289867d1897362216c1ef22c
jpbarto <jason.p.barto@gmail.com>
2024-10-19 00:33:06 +0100

    added the basic elements for the updated playground

--- appres-vite/src/main.ts
+++ appres-vite/src/main.ts
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 import PrimeButton from 'primevue/button';
+import PrimeTabs from 'primevue/tabs';
+import PrimeTabList from 'primevue/tablist';
+import PrimeTab from 'primevue/tab';
+import PrimeTabPanels from 'primevue/tabpanels';
+import PrimeTabPanel from 'primevue/tabpanel';
+
 import './style.css'
 import App from './App.vue'
 
@@ -16,5 +22,10 @@ app.use(PrimeVue, {
 });
 
 app.component('PrimeButton', PrimeButton);
+app.component('PrimeTabs', PrimeTabs);
+app.component('PrimeTabList', PrimeTabList);
+app.component('PrimeTab', PrimeTab);
+app.component('PrimeTabPanels', PrimeTabPanels);
+app.component('PrimeTabPanel', PrimeTabPanel);
 
 app.mount('#app')
