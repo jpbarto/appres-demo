370c28c48d98310e2035052948b4c1afb6b1b035
jpbarto <jason.p.barto@gmail.com>
2024-10-21 23:53:47 +0100

    got vite plus primevue working

--- appres-vite/src/App.vue
+++ appres-vite/src/App.vue
@@ -1,6 +1,58 @@
-<script setup lang="ts">
+<script setup>
+import { ref } from "vue";
+import LoadStats from './components/LoadStats.vue'
+
 import * as jsNetwork from './assets/js/Network.js';
 import * as StatsMan from './assets/js/StatsMan.js';
+
+const items = ref([])
+const altitems=    [{
+        label: 'Resilience ',
+        root: true,
+        items: [
+            [
+                {
+                    items: [
+                        { label: 'Features', icon: 'pi pi-list', subtext: 'Subtext of item' },
+                        { label: 'Customers', icon: 'pi pi-users', subtext: 'Subtext of item' },
+                        { label: 'Case Studies', icon: 'pi pi-file', subtext: 'Subtext of item' }
+                    ]
+                }
+            ],
+            [
+                {
+                    items: [
+                        { label: 'Solutions', icon: 'pi pi-shield', subtext: 'Subtext of item' },
+                        { label: 'Faq', icon: 'pi pi-question', subtext: 'Subtext of item' },
+                        { label: 'Library', icon: 'pi pi-search', subtext: 'Subtext of item' }
+                    ]
+                }
+            ],
+            [
+                {
+                    items: [
+                        { label: 'Community', icon: 'pi pi-comments', subtext: 'Subtext of item' },
+                        { label: 'Rewards', icon: 'pi pi-star', subtext: 'Subtext of item' },
+                        { label: 'Investors', icon: 'pi pi-globe', subtext: 'Subtext of item' }
+                    ]
+                }
+            ],
+            [
+                {
+                    items: [{ image: 'https://primefaces.org/cdn/primevue/images/uikit/uikit-system.png', label: 'GET STARTED', subtext: 'Build spectacular apps in no time.' }]
+                }
+            ]
+        ]
+    },
+    {
+        label: 'Resources',
+        root: true
+    },
+    {
+        label: 'Contact',
+        root: true
+    }
+];
 
 const network = new jsNetwork.Network(1, 1);
 
@@ -32,7 +84,7 @@ statsMan.addNode(appsvr);
 statsMan.addNode(websvr);
 statsMan.addNode(gateway);
 statsMan.addNode(loadGen);
-// const loadStatsBoard = statsMan.loadStatsBoard;
+const loadStatsBoard = statsMan.loadStatsBoard;
 // const dataStatsBoard = statsMan.nodeStats[datasvr.nodeName];
 // const appStatsBoard = statsMan.nodeStats[appsvr.nodeName];
 // const webStatsBoard = statsMan.nodeStats[websvr.nodeName];
@@ -41,8 +93,13 @@ statsMan.addNode(loadGen);
 </script>
 
 <template>
-  <header>Resilience Playground</header>
-  <PrimeButton label="Vertical / horizontal" />
+        <header style="border-radius: 3rem">Resilience Playground</header>
+
+  <div id="dashboard">
+    <LoadStats worker="loadGen" stats-board="loadStatsBoard"/>
+  </div>
+
+  <PrimeDivider />
 
   <div class="card">
     <PrimeTabs value="0">
